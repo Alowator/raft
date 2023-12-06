4e9a8e9e5cf3220f89beadc77b38ccd2a48fe707
Mark Bukhner <markbuxner@mail.ru>
2023-12-06 12:00:38 +0700

    Add ZooKeeper conf example

--- node/lock_provider.py
+++ node/lock_provider.py
@@ -6,25 +6,20 @@ class LockProvider:
 
     def acquire(self, lockID, clientID, currentTime):
         existingLock = self._locks.get(lockID, None)
-        # Auto-unlock old lock
         if existingLock is not None:
             if currentTime - existingLock[1] > self.UNLOCK_TIME:
                 existingLock = None
-        # Acquire lock if possible
         if existingLock is None or existingLock[0] == clientID:
             self._locks[lockID] = (clientID, currentTime)
             return True
-        # Lock already acquired by someone else
         return False
 
     def prolongate(self, clientID, currentTime):
         for lockID in list(self._locks):
             lockClientID, lockTime = self._locks[lockID]
-
             if currentTime - lockTime > self.UNLOCK_TIME:
                 del self._locks[lockID]
                 continue
-
             if lockClientID == clientID:
                 self._locks[lockID] = (clientID, currentTime)
 
